472064c9a614b979e2cd8606640f2d4cf8b4ca99
novette <hi@tsundere.click>
2025-05-28 23:25:56 -0300

    Initial commit.

---
+++ src/Main.kt
@@ -0,0 +1,49 @@
+// TODO: Documentate the code.
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun main() {
+    var x1: Double?
+
+    var x2: Double?
+
+    val a: Double = readlnOrNull()
+        ?.takeIf { it.isNotBlank() && it.contains(Regex("-?\\d+(\\.\\d+)?")
+        ) }
+        ?.toDouble()
+        ?: 0.0
+
+    if (a == 0.0) {
+        println("A cannot be zero!")
+        return
+    }
+
+    val b: Double = readlnOrNull()
+        ?.takeIf { it.isNotBlank() && it.contains(Regex("-?\\d+(\\.\\d+)?")
+        ) }
+        ?.toDouble()
+        ?: 0.0
+
+    val c: Double = readlnOrNull()
+        ?.takeIf { it.isNotBlank() && it.contains(Regex("-?\\d+(\\.\\d+)?")
+        ) }
+        ?.toDouble()
+        ?: 0.0
+
+    val delta = (b.pow(2.0) - 4 * a * c)
+
+    when {
+        delta > 0.0 -> {
+            x1 = (-(b) + sqrt(delta)) / (2.0 * a)
+            x2 = (-(b) - sqrt(delta)) / (2.0 * a)
+            println("${x1}, ${x2}, delta: $delta")
+        }
+        delta == 0.0 -> {
+            println("There's only one root.")
+        }
+        delta < 0.0 -> {
+            println("There's no real roots.")
+        }
+    }
+}
